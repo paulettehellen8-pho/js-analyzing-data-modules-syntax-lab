59038ff22b7409a7014c02043f88008fbbe7a3d9
Paulette Hellen Obimbo <paulettehellen8@gmail.com>
2026-07-08 13:01:58 +0300

    merged all usernames into one

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+require("datejs");
 
+//Create a function that merges all usernames into one.
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+  };
 
+  //Loop through args to isolate each array
+  for (const user of args) {
+    //Merge arrays together using the spread operator
+    combinedObject.users = [...combinedObject.users, ...user];
+  }
+  //Get today's date using datejs, formatted as M/d/yyyy
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
